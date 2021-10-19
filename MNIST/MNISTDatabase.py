9edc651346f37d66cfe3c45522998b29b2df9189
Dunspa <jlgallego99@correo.ugr.es>
2021-10-19 17:31:31 +0200

    Inicio practica MNIST

---
+++ MNIST/MNISTDatabase.py
@@ -0,0 +1,14 @@
+import urllib
+
+MNIST_URL = "http://yann.lecun.com/exdb/mnist/"
+
+# Training data
+trainingImages = "train-images-idx3-ubyte.gz"
+trainingLabels = "train-labels-idx1-ubyte.gz"
+
+# Test data
+testImages = "t10k-images-idx3-ubyte.gz"
+testLabels = "t10k-labels-idx1-ubyte.gz"
+
+def download(urlString, filename):
+	pass
