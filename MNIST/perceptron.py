05d1956007ee28b38a51e5ffa8cb644d7fd289e8
Dunspa <jlgallego99@correo.ugr.es>
2021-11-10 15:50:17 +0100

    Comprobar que no existen los datos y descargarlos si es asi

--- MNIST/perceptron.py
+++ MNIST/perceptron.py
@@ -1,7 +1,22 @@
+import os
+import logging
 import tensorflow as tf
-import keras
-from keras.utils import to_categorical
+import tensorflow.keras
+from tensorflow.keras.utils import to_categorical
 import mnist_db as mnist
+
+logging.basicConfig(level = logging.INFO)
+
+# Descargar datos si no existen
+if not os.path.isdir("./data/"):
+    logging.info("El directorio de datos no existe, creando...")
+    os.mkdir("./data/")
+    mnist.descargar_mnist('./data/')
+elif not os.listdir('./data/'):
+    logging.info("El directorio de datos está vacío")
+    mnist.descargar_mnist('./data/')
+else:
+    logging.info("Los datos ya están descargados")
 
 (training_images, training_labels, test_images, test_labels) = mnist.leer_mnist("./data/")
 
