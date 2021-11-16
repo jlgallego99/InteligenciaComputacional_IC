bbcef95b696c1ea8fea18a307426355681da2d90
Dunspa <jlgallego99@correo.ugr.es>
2021-11-16 17:53:24 +0100

    Distinguir entre perceptron single y multi

--- MNIST/perceptron.py
+++ MNIST/perceptron.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import logging
 from tensorflow.keras import models
 from tensorflow.keras import layers
@@ -6,6 +7,10 @@ from tensorflow.keras.utils import to_categorical
 import mnist_db as mnist
 
 logging.basicConfig(level = logging.INFO)
+
+if len(sys.argv) == 1:
+	print("Error, se debe indicar como parámetro el tipo de perceptron: single/multi")
+	exit(-1)
 
 # Descargar datos si no existen
 if not os.path.isdir("./data/"):
@@ -27,14 +32,21 @@ test_labels = to_categorical(test_labels)
 # Definir la topología de la red neuronal
 # Una capa oculta de unidades lineales rectificadas y una capa de salida softmax
 network = models.Sequential()
-#network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
-network.add(layers.Dense(10, activation='softmax'))
+
+if sys.argv[1] == "single":
+	network.add(layers.Dense(10, activation='softmax'))
+elif sys.argv[1] == "multi":
+	network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,), kernel_initializer='random_uniform'))
+	network.add(layers.Dense(10, activation='softmax'))
+else:
+	print("Error, parámetro incorrecto")
+	exit(-1)
 
 # Preparar red especificando: función de error, optimizador y las métricas para evaluar su funcionamiento
 network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Entrenar la red
-network.fit(training_images, training_labels, epochs=5, batch_size=128)
+network.fit(training_images, training_labels, epochs=10, batch_size=128)
 
 # Evaluar la red sobre el conjunto de prueba
 print("Evaluando red neuronal...")
