d0b3c005e51cf17a21b45149217e45de9bd14084
Dunspa <jlgallego99@correo.ugr.es>
2021-11-24 20:22:07 +0100

    Redirigir labels a fichero de texto

--- MNIST/perceptron.py
+++ MNIST/perceptron.py
@@ -5,12 +5,13 @@ from tensorflow.keras import models
 from tensorflow.keras import layers
 from tensorflow.keras.utils import to_categorical
 import mnist_db as mnist
+import numpy as np
 
-logging.basicConfig(level = logging.INFO)
+logging.basicConfig(level=logging.INFO)
 
 if len(sys.argv) == 1:
-	print("Error, se debe indicar como parámetro el tipo de perceptron: single/multi")
-	exit(-1)
+    print("Error, se debe indicar como parámetro el tipo de perceptron: single/multi")
+    exit(-1)
 
 # Descargar datos si no existen
 if not os.path.isdir("./data/"):
@@ -34,13 +35,13 @@ test_labels = to_categorical(test_labels)
 network = models.Sequential()
 
 if sys.argv[1] == "single":
-	network.add(layers.Dense(10, activation='softmax'))
+    network.add(layers.Dense(10, activation='softmax'))
 elif sys.argv[1] == "multi":
-	network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,), kernel_initializer='random_uniform'))
-	network.add(layers.Dense(10, activation='softmax'))
+    network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,), kernel_initializer='random_uniform'))
+    network.add(layers.Dense(10, activation='softmax'))
 else:
-	print("Error, parámetro incorrecto")
-	exit(-1)
+    print("Error, parámetro incorrecto")
+    exit(-1)
 
 # Preparar red especificando: función de error, optimizador y las métricas para evaluar su funcionamiento
 network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
@@ -50,6 +51,14 @@ network.fit(training_images, training_labels, epochs=10, batch_size=128)
 
 # Evaluar la red sobre el conjunto de prueba
 print("Evaluando red neuronal...")
-test_loss, test_acc = network.evaluate(test_images, test_labels)
-print("Precisión sobre el conjunto de prueba: ", test_acc * 100, "%")
-print("Error sobre el conjunto de prueba: ", 100.0 - (test_acc * 100), "%")
+_, test_acc = network.evaluate(test_images, test_labels)
+labels_prueba = network.predict(test_images)
+print("Precisión sobre el conjunto de prueba: ", test_acc * 100.0, "%")
+print("Error sobre el conjunto de prueba: ", 100.0 - (test_acc * 100.0), "%")
+
+# Guardar etiquetas asignadas a los casos del conjunto de prueba en un fichero txt
+sourceFile = open('labels.txt', 'w')
+for label in labels_prueba:
+    print(np.argmax(label), end='', file=sourceFile)
+
+sourceFile.close()
